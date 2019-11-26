43fd8b5d3afea8d03e992af50022fe27a857c951
Judah Barton <judah.barton@yahoo.com>
2019-11-26 10:41:44 -0800

    add all the information about a doctor

--- src/main.js
+++ src/main.js
@@ -25,6 +25,9 @@ $(document).ready(function(){
       $("#yourSymptoms").text(`Your Symptoms: ${problem}`)
       $('#doctor').text(`Doctor: ${answer.data[1].profile.first_name} ${answer.data[1].profile.last_name}`);
       $('#doctorAddress').text(`Address: ${answer.data[1].practices[0].visit_address.street} ${answer.data[1].practices[0].visit_address.city} ${answer.data[1].practices[0].visit_address.state}  ${answer.data[1].practices[0].visit_address.zip}`);
+      $('#doctornumber').text(`Phone number: ${answer.data[1].practices[0].phones[0].number}`);
+      $('#doctorWebsite').text(`Website: ${answer.data[1].practices[0].website}`);
+      $('#doctorAccept').text(`Accepting new patients: ${answer.data[1].practices[0].accepts_new_patients}`);
       console.log(randomDoctor);
     }
 
