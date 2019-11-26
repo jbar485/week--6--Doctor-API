c93dd74fa80eb6c561f0f72d68ddd48a62990597
Judah Barton <judah.barton@yahoo.com>
2019-11-26 14:52:01 -0800

    add code to work on searching for name

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,7 +1,7 @@
 export class DoctorService {
-  async getDoctorAnswer(problem, specificDoctor) {
+  async getDoctorAnswer(problem, name) {
     try {
-      let answer = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.3479%2C122.6806&query=${problem}&query=${specificDoctor}&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let answer = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}location=or-portland&user_location=45.3479%2C122.6806&query=${problem}&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let doctorList = await answer.json();
       return doctorList;
     }catch(error) {
--- src/main.js
+++ src/main.js
@@ -7,28 +7,25 @@ $(document).ready(function(){
   $("#answerButton").click(function(event){
     event.preventDefault();
     const problem = $("#symptoms").val();
-    const specificDoctor = $("#doctorName").val();
+    const name = $("#doctorName").val();
     $("#symptoms").val("");
     $("#doctorName").val("");
 
     (async () => {
       let doctorService = new DoctorService();
-      const answer = await doctorService.getDoctorAnswer(problem, specificDoctor);
+      const answer = await doctorService.getDoctorAnswer(problem, name);
       getAnswer(answer);
-      console.log(doctorService);
     })();
 
 
 
     function getAnswer(answer) {
-      let randomDoctor = (`${answer.data[1].profile.first_name}`);
       $("#yourSymptoms").text(`Your Symptoms: ${problem}`)
       $('#doctor').text(`Doctor: ${answer.data[1].profile.first_name} ${answer.data[1].profile.last_name}`);
       $('#doctorAddress').text(`Address: ${answer.data[1].practices[0].visit_address.street} ${answer.data[1].practices[0].visit_address.city} ${answer.data[1].practices[0].visit_address.state}  ${answer.data[1].practices[0].visit_address.zip}`);
       $('#doctornumber').text(`Phone number: ${answer.data[1].practices[0].phones[0].number}`);
       $('#doctorWebsite').text(`Website: ${answer.data[1].practices[0].website}`);
       $('#doctorAccept').text(`Accepting new patients: ${answer.data[1].practices[0].accepts_new_patients}`);
-      console.log(randomDoctor);
     }
 
   });
