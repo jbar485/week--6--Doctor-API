c0690770a01fbb35f2286d5709b857c234a9816e
Judah Barton <judah.barton@yahoo.com>
2019-11-26 10:24:36 -0800

    add a working address and symptoms

--- src/main.js
+++ src/main.js
@@ -22,8 +22,9 @@ $(document).ready(function(){
 
     function getAnswer(answer) {
       let randomDoctor = (`${answer.data[1].profile.first_name}`);
-      $("#yourSymptons").text(`Your Symptoms: ${problem}`)
-      $('.doctor').text(`Doctor: ${answer.data[1].profile.first_name} ${answer.data[1].profile.last_name}`);
+      $("#yourSymptoms").text(`Your Symptoms: ${problem}`)
+      $('#doctor').text(`Doctor: ${answer.data[1].profile.first_name} ${answer.data[1].profile.last_name}`);
+      $('#doctorAddress').text(`Address: ${answer.data[1].practices[0].visit_address.street} ${answer.data[1].practices[0].visit_address.city} ${answer.data[1].practices[0].visit_address.state}  ${answer.data[1].practices[0].visit_address.zip}`);
       console.log(randomDoctor);
     }
 
